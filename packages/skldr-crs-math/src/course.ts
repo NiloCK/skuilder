385b3de2aa926d408d53e8f5d191328b294dc29b
nilock <colinkennedy@hotmail.com>
2017-07-02 17:17:46 -0600

    Fixes to the tsconfig and course export
    Types now consumable by "packages/skuilder"

--- packages/skldr-crs-math/src/course.ts
+++ packages/skldr-crs-math/src/course.ts
@@ -1,15 +1,24 @@
-import Multiplication from './questions/multiplication'
-import { Division } from './questions/division'
+import MultiplicationType from './questions/multiplication'
+import DivisionType from './questions/division'
 
 // export.Multiplication = Multiplication;
 // export.Division = Division;
 
 module Course {
-    export type Multiplication = Multiplication;
-    export var Multiplication = new Multiplication();
+    export type Multiplication = MultiplicationType;
+    // export var Multiplication: typeof MultiplicationType;
 
-    export type Division = Division;
-    export var Division = new Division();
+    export type Division = DivisionType;
+    // export var Division: typeof DivisionType;
+    // export var Multiplication = new Multiplication();
+
+    // export type Division = Division;
+    // export var Division = new Division();
+
+    export const types = [
+        MultiplicationType,
+        DivisionType
+    ]
 }
 
-export = Course;
+export default Course;
