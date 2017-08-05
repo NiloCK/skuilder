0f78c5c97500b35886fa4f3f096f9f361a89bada
nilock <colinkennedy@hotmail.com>
2017-08-05 14:34:29 -0600

    Add subtraction question to crs export

--- packages/skldr-crs-math/src/course.ts
+++ packages/skldr-crs-math/src/course.ts
@@ -1,12 +1,14 @@
 import MultiplicationType from './questions/multiplication'
 import DivisionType from './questions/division'
 import Addition from './questions/addition'
+import Subtraction from './questions/subtraction'
 
 module Course {
     export const types = [
         MultiplicationType,
         DivisionType,
-        Addition
+        Addition,
+        Subtraction
     ]
 }
 
