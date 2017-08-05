839f794a81f9008a692ec137fd8f9eae248e1346
nilock <colinkennedy@hotmail.com>
2017-08-04 18:59:18 -0600

    Add addition prob to course export, remove cruft

--- packages/skldr-crs-math/src/course.ts
+++ packages/skldr-crs-math/src/course.ts
@@ -1,23 +1,12 @@
 import MultiplicationType from './questions/multiplication'
 import DivisionType from './questions/division'
-
-// export.Multiplication = Multiplication;
-// export.Division = Division;
+import Addition from './questions/addition'
 
 module Course {
-    export type Multiplication = MultiplicationType;
-    // export var Multiplication: typeof MultiplicationType;
-
-    export type Division = DivisionType;
-    // export var Division: typeof DivisionType;
-    // export var Multiplication = new Multiplication();
-
-    // export type Division = Division;
-    // export var Division = new Division();
-
     export const types = [
         MultiplicationType,
-        DivisionType
+        DivisionType,
+        Addition
     ]
 }
 
