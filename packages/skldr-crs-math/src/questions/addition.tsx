c5c05b535f3721bc26de3f4a098f51dd222ec010
nilock <colinkennedy@hotmail.com>
2017-08-04 18:58:21 -0600

    Make a standard Addition Problem

--- packages/skldr-crs-math/src/questions/addition.tsx
+++ packages/skldr-crs-math/src/questions/addition.tsx
@@ -1,28 +1,58 @@
 import * as RX from 'reactxp';
+import { QuestionView, QuestionViewProps, Question } from 'skldr-course-base'
 
-export interface SingleDigitAdditionProblemProps extends RX.CommonProps {
-    a: number;
-    b: number;
+const styles = {
+    form: RX.Styles.createViewStyle({
+        flexDirection: "row",
+        padding: 15
+    })
 }
 
-class SingleDigitAdditionProblem extends RX.Component<SingleDigitAdditionProblemProps, null> {
-    a: number;
-    b: number;
+function getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}; //todo move this somewhere sensible (or find a lib?)
 
-    constructor(props: SingleDigitAdditionProblemProps) {
-        super(props);
+export class SingleDigitAdditionQuestion extends Question {
+    a: number = getRandomInt(0, 10);
+    b: number = getRandomInt(0, 10);
 
-        this.a = props.a;
-        this.b = props.b;
+    isCorrect(answer: number) {
+        return this.a + this.b == answer;
+    }
+}
+export interface SingleDigitAdditionQuestionProps extends QuestionViewProps {
+    question: SingleDigitAdditionQuestion;
+}
+
+class SingleDigitAdditionProblemView extends QuestionView<SingleDigitAdditionQuestionProps>{
+    static getProps(): SingleDigitAdditionQuestionProps {
+        return {
+            onanswer: null,
+            question: new SingleDigitAdditionQuestion()
+        }
     }
 
     render() {
+        let { question } = this.props;
+
         return (
             <RX.View>
-                {this.a} + {this.b} = ___
+
+                <RX.View style={styles.form}>
+
+                    <div id="question">
+                        {question.a} + {question.b} =&nbsp;
+                    </div>
+                    <form onSubmit={this.submit.bind(this)}>
+                        <input className="mousetrap"
+                            autoFocus
+                            id="answer" type="number" autoComplete={false} />
+                    </form>
+
+                </RX.View>
             </RX.View>
         );
     }
 }
 
-export default SingleDigitAdditionProblem;
+export default SingleDigitAdditionProblemView;
